f545329f845b16a8c53a5f4d95c374aa284fa442
Syd Bailey <sydabailey@gmail.com>
2019-07-25 23:55:28 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,13 @@
 def unsafe?(speed)
+	if speed | speed 60
+    return true
+else
+    false
+		end
+	end
 
+
+ def not_safe?(speed)
+	 	speed | speed 60 ? true : false
+	end
 end
-
-
-
-def not_safe?(speed)
-	
-end
-	
-
-
